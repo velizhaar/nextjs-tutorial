aeccfb52335eb85eb49493c7c8ed0cbd79fa2860
velizhaar <velizhaar@gmail.com>
2021-03-18 00:25:44 +0700

    error

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "../../styles/Jobs.module.css";
 
 export const getStaticProps = async () => {
@@ -5,22 +6,20 @@ export const getStaticProps = async () => {
   const data = await res.json();
 
   return {
-    props: { ninjas: data },
+    props: { ninja: data },
   };
 };
 
 const Ninjas = ({ ninjas }) => {
-  console.log(ninjas);
-
   return (
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map((ninja) => (
-        <div key={ninja.id}>
+        <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ninja.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   );
