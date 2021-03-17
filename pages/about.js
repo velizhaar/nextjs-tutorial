ceb83fd2f49cb963ee5499f8cf59e5c113dca0c9
velizhaar <velizhaar@gmail.com>
2021-03-17 16:40:57 +0700

    linking between pages and creating a layout

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, aut?
+        Exercitationem ad culpa possimus nam distinctio, nihil incidunt illo
+        quasi amet aliquam nisi. Tempore, officia! Nostrum dolore ipsam debitis
+        quaerat!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, quas
+        saepe modi sequi odit dolore, animi enim rerum cumque reiciendis ullam
+        velit ipsa architecto suscipit placeat a! Cumque, nostrum obcaecati!
+      </p>
+    </div>
+  );
+};
+
+export default About;
