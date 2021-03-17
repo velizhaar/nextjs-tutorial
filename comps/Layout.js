82d68d198561ac8b28b2b397f1d72c1354e0984a
velizhaar <velizhaar@gmail.com>
2021-03-17 23:05:13 +0700

    fetching data (getStaticProps)

--- comps/Layout.js
+++ comps/Layout.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div class="content">
+    <div className="content">
       <Navbar />
       {children}
       <Footer />
