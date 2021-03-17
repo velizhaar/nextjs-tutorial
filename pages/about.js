ee6d5456b4553d655bc6b062e176db94d7e66e33
velizhaar <velizhaar@gmail.com>
2021-03-17 22:10:45 +0700

    adding images and metadata

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,27 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, aut?
-        Exercitationem ad culpa possimus nam distinctio, nihil incidunt illo
-        quasi amet aliquam nisi. Tempore, officia! Nostrum dolore ipsam debitis
-        quaerat!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, quas
-        saepe modi sequi odit dolore, animi enim rerum cumque reiciendis ullam
-        velit ipsa architecto suscipit placeat a! Cumque, nostrum obcaecati!
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum, aut?
+          Exercitationem ad culpa possimus nam distinctio, nihil incidunt illo
+          quasi amet aliquam nisi. Tempore, officia! Nostrum dolore ipsam
+          debitis quaerat!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, quas
+          saepe modi sequi odit dolore, animi enim rerum cumque reiciendis ullam
+          velit ipsa architecto suscipit placeat a! Cumque, nostrum obcaecati!
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-        consectetur eveniet voluptas eaque maxime reprehenderit porro mollitia,
-        consequatur quia quibusdam modi accusamus. Laboriosam, ratione ex odio
-        quibusdam optio sed id.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ut
-        facilis tempore iure quia omnis eligendi quidem, reiciendis magni
-        consectetur, minima necessitatibus dignissimos ratione et unde soluta
-        doloremque. Voluptatibus, recusandae
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+          consectetur eveniet voluptas eaque maxime reprehenderit porro
+          mollitia, consequatur quia quibusdam modi accusamus. Laboriosam,
+          ratione ex odio quibusdam optio sed id.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ut
+          facilis tempore iure quia omnis eligendi quidem, reiciendis magni
+          consectetur, minima necessitatibus dignissimos ratione et unde soluta
+          doloremque. Voluptatibus, recusandae
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
